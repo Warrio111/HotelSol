2c2ed1e3763b95cae7f6b0756b59e3b0c9e3a2d3
Random User <104085014+mratomo@users.noreply.github.com>
2023-10-26 14:07:43 +0200

    Se generan las estructuras de los controladores Cliente y Reserva para implementaciones iniciales y de prueba con codigo comentado de lo que falta por implementar.

---
+++ HotelSolRepo/Controlador/ClienteController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using HotelSolRepo.Modelo;  
+
+namespace HotelSolRepo.Controlador
+{
+    public class ClienteController
+    {
+        private string rutaArchivoXmlClientes = @"ruta\al\archivo\clientes.xml";
+
+        // Método para registrar un nuevo cliente
+        public void RegistrarCliente(Cliente nuevoCliente)
+        {
+            XDocument doc = XDocument.Load(rutaArchivoXmlClientes);
+            // Implementar lógica para añadir un nuevo cliente al archivo XML
+        }
+
+        // Método para actualizar la información de un cliente existente
+        public void ActualizarCliente(Cliente clienteActualizado)
+        {
+            // Implementar lógica para actualizar la información del cliente en el archivo XML
+        }
+
+        // Método para eliminar un cliente
+        public void EliminarCliente(string clienteId)
+        {
+            // Implementar lógica para eliminar un cliente del archivo XML
+        }
+
+        // Método para obtener información de un cliente específico
+        public Cliente ObtenerCliente(string clienteId)
+        {
+            // Implementar lógica para obtener la información de un cliente específico del archivo XML
+            return new Cliente(); // Devuelve un objeto Cliente con la información recuperada
+        }
+
+        // Otros métodos adicionales según sea necesario
+    }
+}
---
+++ HotelSolRepo/Controlador/ReservaController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using HotelSolRepo.Modelo;  
+
+namespace HotelSolRepo.Controlador
+{
+    public class ReservaController
+    {
+        private string rutaArchivoXml = @"ruta\al\archivo\reservas.xml";
+
+        // Método para comprobar disponibilidad de habitaciones
+        public bool ComprobarDisponibilidad(DateTime fechaInicio, DateTime fechaFin)
+        {
+            XDocument doc = XDocument.Load(rutaArchivoXml);
+            // Implementar lógica para comprobar disponibilidad
+            return true;  // Devuelve true si hay disponibilidad, de lo contrario false
+        }
+
+        // Método para registrar un nuevo cliente
+        public void RegistrarCliente(Cliente nuevoCliente)
+        {
+            // Implementar lógica para registrar un nuevo cliente en el archivo XML
+        }
+
+        // Método para hacer una nueva reserva
+        public void HacerReserva(Reserva nuevaReserva)
+        {
+            // Implementar lógica para añadir una nueva reserva al archivo XML
+        }
+
+        // Otros métodos adicionales como cancelar reserva, etc.
+    }
+}
