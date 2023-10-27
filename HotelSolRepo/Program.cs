b235ed83eaa6a7f6b0d61f3a9f49a149aa05911e
Random User <104085014+mratomo@users.noreply.github.com>
2023-10-27 19:53:47 +0200

    Creada ventana de vista para las funcionalidades

--- HotelSolRepo/Program.cs
+++ HotelSolRepo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HotelSolRepo.Vista;
 
 namespace HotelSolRepo
 {
@@ -16,7 +17,7 @@ namespace HotelSolRepo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormPrincipal());
         }
     }
 }
---
+++ HotelSolRepo/Vista/ImportarForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelSolRepo.Vista
+{
+    public partial class ImportarForm : Form
+    {
+        public ImportarForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
