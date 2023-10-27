d8771eaf50c873d609c6b030e527973416581571
Random User <104085014+mratomo@users.noreply.github.com>
2023-10-27 21:41:16 +0200

    Implementacion parcial de menus

--- HotelSolRepo/Vista/ReservaForm.cs
+++ HotelSolRepo/Vista/ReservaForm.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
-using HotelSolRepo.Vista;  // Importe el espacio de nombres si los formularios están en diferentes espacios de nombres
+using HotelSolRepo.Vista;  
 
 namespace HotelSolRepo.Vista
 {
@@ -16,13 +16,13 @@ namespace HotelSolRepo.Vista
 
         private void BtnAbrirLoginForm_Click(object sender, EventArgs e)
         {
-            // Suponiendo que FormPrincipal es el formulario padre y tiene un método público MostrarFormulario
+           
             ((FormPrincipal)this.ParentForm).MostrarFormulario(new LoginForm());
         }
 
         private void BtnAbrirRegisterForm_Click(object sender, EventArgs e)
         {
-            // Suponiendo que FormPrincipal es el formulario padre y tiene un método público MostrarFormulario
+           
             ((FormPrincipal)this.ParentForm).MostrarFormulario(new RegisterForm());
         }
     }
