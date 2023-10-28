54b6e97519f7b6c08dc59261fbfd53a4ae8c17c3
Robert Benavides <robertbenavidesgomez@gmail.com>
2023-10-28 23:30:07 +0200

    Investigar los problemas de XML

--- HotelSolRepo/Vista/ExportarForm.cs
+++ HotelSolRepo/Vista/ExportarForm.cs
@@ -69,41 +69,15 @@ namespace HotelSolRepo.Vista
                                 }
                             }
                         }
-                        try
-                        {
-                            using (FileStream stream = new FileStream(rutaArchivoXml, FileMode.Create))
-                            {
-                                // Serializa los datos y escribe el archivo XML
-                                serializer.Serialize(stream, serializeXMLWrapperType);
-                            }
-
-                            MessageBox.Show("Exportación exitosa.");
-                        }
-                        catch (Exception ex)
-                        {
-                            MessageBox.Show("Error al exportar los datos: " + ex.Message);
-                        }
                     }
                     else if (result == DialogResult.No)
                     {
                         // El usuario desea añadir datos al archivo donde corresponda
-
-                        // Cargar los datos existentes del archivo
-                        List<object> existingData = new List<object>();
-                        using (FileStream existingFileStream = new FileStream(rutaArchivoXml, FileMode.Open))
-                        {
-                            while (existingFileStream.Position < existingFileStream.Length)
-                            {
-                                object existingItem = serializer.Deserialize(existingFileStream);
-                                existingData.Add(existingItem);
-                            }
-                        }
-
-                        // Crear una nueva instancia para almacenar los nuevos datos
-                        List<object> newData = new List<object>();
                         if (dataForm is ReservaForm)
                         {
                             ReservaForm reservaForm = (ReservaForm)dataForm;
+
+                            // Crear una nueva instancia del tipo exportXmlWrapperType y asignarle los datos
                             serializeXMLWrapperType = Activator.CreateInstance(exportXmlWrapperType);
                             foreach (PropertyInfo propertyInfo in exportXmlWrapperType.GetProperties())
                             {
@@ -114,29 +88,73 @@ namespace HotelSolRepo.Vista
                                     propertyInfo.SetValue(serializeXMLWrapperType, value);
                                 }
                             }
-
-                            // Agregar los nuevos datos a la lista
-                            newData.Add(serializeXMLWrapperType);
-                            newData.AddRange(existingData);
-
-                            // Escribir la lista actualizada al archivo
+                            // Cargar los datos existentes del archivo
+                            List<object> existingData = new List<object>();
+                            using (FileStream existingFileStream = new FileStream(rutaArchivoXml, FileMode.Open))
+                            {
+                                while (existingFileStream.Position < existingFileStream.Length)
+                                {
+                                    object existingItem = serializer.Deserialize(existingFileStream);
+                                    existingData.Add(existingItem);
+                                }
+                            }
+                            // Agregar los nuevos datos a la lista de datos existentes
+                            existingData.Add(serializeXMLWrapperType);
+                            // Sobrescribir el archivo con la lista actualizada de datos
                             using (FileStream updatedFileStream = new FileStream(rutaArchivoXml, FileMode.Create))
                             {
-                                foreach (var item in newData)
+                                foreach (var item in existingData)
                                 {
                                     serializer.Serialize(updatedFileStream, item);
                                 }
                             }
 
                             MessageBox.Show("Exportación exitosa.");
+                            return;
+
                         }
                     }
-                
+                }
+                else
+                {
+                    // El archivo no existe, simplemente guarda los datos
+                    if (dataForm is ReservaForm)
+                    {
+                        ReservaForm reservaForm = (ReservaForm)dataForm;
+                        // Aquí creamos una instancia del tipo exportXmlWrapperType
+                        serializeXMLWrapperType = Activator.CreateInstance(exportXmlWrapperType);
+                        foreach (PropertyInfo propertyInfo in exportXmlWrapperType.GetProperties())
+                        {
+                            PropertyInfo sourceProperty = reservaForm.RealizarExportDesdeReservas().GetType().GetProperty(propertyInfo.Name);
+                            if (sourceProperty != null)
+                            {
+                                object value = sourceProperty.GetValue(reservaForm.RealizarExportDesdeReservas());
+                                propertyInfo.SetValue(serializeXMLWrapperType, value);
+                            }
+                        }
+                    }
+
+                }
+                try
+                {
+                    using (FileStream stream = new FileStream(rutaArchivoXml, FileMode.Create))
+                    {
+                        // Serializa los datos y escribe el archivo XML
+                        serializer.Serialize(stream, serializeXMLWrapperType);
+                    }
+                    MessageBox.Show("Exportación exitosa.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al exportar los datos: " + ex.Message);
                 }
 
-               
-
             }
+            else
+            {
+                MessageBox.Show("Exportación cancelada.");
+            }
+            
         }
 
     }
